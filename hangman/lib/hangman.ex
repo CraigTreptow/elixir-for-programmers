1fc1d6b9dc3c9918e3bdbc3efdb2df5539560217
Craig Treptow <craig.treptow@gmail.com>
2018-10-24 18:24:57 -0500

    API now uses GenServer to insulate users from the implementation details

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,12 +1,16 @@
 defmodule Hangman do
   # public API only
 
-  #alias Hangman.Game, as: Wombat
-  # Leave the as: part off and the alias is just Game
-  alias Hangman.Game
+  def new_game() do
+    Hangman.Server.start_link()
+  end
 
-  defdelegate new_game(),             to: Game
-  defdelegate tally(game),            to: Game
-  defdelegate make_move(game, guess), to: Game
+  def tally(game_pid) do
+    GenServer.call(game_pid, { :tally })
+  end
+
+  def make_move(game_pid, guess) do
+    GenServer.call(game_pid, { :make_move, guess })
+  end
 
 end
