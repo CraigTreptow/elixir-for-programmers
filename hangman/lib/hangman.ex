1c77b88708f8321d428a8952f78877ef7f2dc7e0
Craig Treptow <craig.treptow@gmail.com>
2018-10-24 17:46:10 -0500

    delegate make_move for a better design

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -5,8 +5,9 @@ defmodule Hangman do
   # Leave the as: part off and the alias is just Game
   alias Hangman.Game
 
-  defdelegate new_game(),  to: Game
-  defdelegate tally(game), to: Game
+  defdelegate new_game(),              to: Game
+  defdelegate tally(game),             to: Game
+  defdelegate  make_move(game, guess), to: Game
 
   def make_move(game, guess) do
     game = Game.make_move(game, guess)
