82af203a6f621e7f964b76f0bb4240603b37cb17
Craig Treptow <craig.treptow@gmail.com>
2018-10-27 08:31:52 -0500

    Updates text client to work with the new server processes for playing the game

--- text_client/lib/text_client/mover.ex
+++ text_client/lib/text_client/mover.ex
@@ -3,8 +3,8 @@ defmodule TextClient.Mover do
   alias TextClient.State
 
   def make_move(game = %State{}) do
-    { new_game_service, new_tally } = Hangman.make_move(game.game_service, game.guess)
-    %State{ game | game_service: new_game_service, tally: new_tally }
+    new_tally = Hangman.make_move(game.game_service, game.guess)
+    %State{ game | tally: new_tally }
   end
 
 end
