5f87c9344f2bb62f772bfeb5896a9042f65e6e5c
Craig Treptow <craig.treptow@gmail.com>
2018-10-28 13:57:43 -0500

    Start hangman server on a remote node (hard coded node name)

--- text_client/lib/text_client/interact.ex
+++ text_client/lib/text_client/interact.ex
@@ -1,9 +1,10 @@
 defmodule TextClient.Interact do
 
   alias TextClient.{Player, State}
+  @hangman_server :hangman@Craig
 
   def start() do
-    Hangman.new_game()
+    new_game()
     |> setup_state()
     |> Player.play()
   end
@@ -13,5 +14,14 @@ defmodule TextClient.Interact do
       game_service: game_service,
       tally:        Hangman.tally(game_service),
     }
+  end
+
+  defp new_game() do
+    Node.connect(@hangman_server)
+    :rpc.call(@hangman_server,
+      Hangman,
+      :new_game,
+      []
+    )
   end
 end
