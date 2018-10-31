231008fcc9e9cbb05d6477877a4a8c36369d89cd
Craig Treptow <craig.treptow@gmail.com>
2018-10-31 15:04:23 -0500

    Adds a new game button with an empty game field page

--- gallows/lib/gallows_web/router.ex
+++ gallows/lib/gallows_web/router.ex
@@ -16,7 +16,8 @@ defmodule GallowsWeb.Router do
   scope "/hangman", GallowsWeb do
     pipe_through :browser # Use the default browser stack
 
-    get "/", HangmanController, :new_game
+    get  "/", HangmanController, :new_game
+    post "/", HangmanController, :create_game
   end
 
   # Other scopes may use custom stacks.
