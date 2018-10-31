656834f6a4e60d6cb8c22924aec66a107fe12218
Craig Treptow <craig.treptow@gmail.com>
2018-10-31 16:17:45 -0500

    adds a simple div (flash) to show resule of guesses

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -2,6 +2,29 @@ defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
   alias GallowsWeb.Router.Helpers, as: Routes
 
+  @responses %{
+    :won          => { :success, "You won!"                  },
+    :lost         => { :danger,  "You lost."                 },
+    :good_guess   => { :success, "Good guess!"               },
+    :bad_guess    => { :warning, "Bad guess."                },
+    :already_used => { :info,    "You already guessed that." },
+  }
+
+  def game_state(state) do
+    @responses[state]
+    |> alert()
+  end
+
+  def alert(nil), do: ""
+  def alert({class, message}) do
+    """
+    <div class="alert alert-#{class}">
+      #{message}
+    </div>
+    """
+    |> raw()
+  end
+
   def word_so_far(tally) do
     tally.letters
     |> Enum.join(" ")
