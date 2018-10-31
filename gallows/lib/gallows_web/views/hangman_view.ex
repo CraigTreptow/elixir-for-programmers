02f06ecff75fe1dce56c5b7639cef55e46e01143
Craig Treptow <craig.treptow@gmail.com>
2018-10-31 15:26:14 -0500

    add view helper for word so far

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,4 +1,9 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
   alias GallowsWeb.Router.Helpers, as: Routes
+
+  def word_so_far(tally) do
+    tally.letters
+    |> Enum.join(" ")
+  end
 end
