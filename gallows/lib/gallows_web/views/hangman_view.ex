fe93d1722d0b254d61a057568c65b912b5b924c1
Craig Treptow <craig.treptow@gmail.com>
2018-11-01 18:38:53 -0500

    Adds hangman SVG with some opacity settings to make things fancy

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -11,6 +11,14 @@ defmodule GallowsWeb.HangmanView do
     button("New Game", to: Routes.hangman_path(conn, :create_game))
   end
 
+  def turn(left, target) when target >= left do
+    "opacity: 1;"
+  end
+
+  def turn(left, _target) do
+    "opacity: 0.1;"
+  end
+
   def used_letters(tally) do
     tally.used
     |> Enum.join(" ")
